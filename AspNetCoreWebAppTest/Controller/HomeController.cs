f632ad07506ebc237cff56cda259cd4a462cb6a0
nacirabdulkadir <121578916+nacirabdulkadir@users.noreply.github.com>
2023-11-14 01:35:07 +0300

    kafka consumer

---
+++ AspNetCoreWebAppTest/Controller/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Text;
+
+namespace AspNetCoreWebAppTest
+{
+    public class HomeController : Controller
+    {
+
+        private readonly KafkaSettings _kafkaSettings;
+
+        public HomeController(IOptions<KafkaSettings> kafkaSettings)
+        {
+            _kafkaSettings = kafkaSettings.Value;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }   
+
+        public string ConsumeKafkaMessage()
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = _kafkaSettings.BootstrapServers,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            StringBuilder sb = new StringBuilder();
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                var topicName = _kafkaSettings.TopicName;
+                c.Subscribe(topicName);
+
+                try
+                {
+                    var cr = c.Consume();
+                    // Mesajı işle, örneğin: Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    sb.Append(cr.Message);
+                }
+                catch (ConsumeException e)
+                {
+                    // Tüketim sırasında hata yönetimi
+                    Console.WriteLine($"Error occured: {e.Error.Reason}");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public class KafkaSettings
+    {
+        public string BootstrapServers { get; set; }
+        public string TopicName { get; set; }
+    }
+}
--- AspNetCoreWebAppTest/Program.cs
+++ AspNetCoreWebAppTest/Program.cs
@@ -1,14 +1,23 @@
+using AspNetCoreWebAppTest;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews(); // MVC için Controller ve View'larý ekler.
+builder.Services.Configure<KafkaSettings>(builder.Configuration.GetSection("Kafka"));
 
 var app = builder.Build();
+
+// Add services to the container.
+
+
+// ... diðer yapýlandýrmalar
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error"); // Hata yönetimi için MVC'de kullanýlan yol
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -20,6 +29,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"); // MVC için varsayýlan route tanýmý
 
 app.Run();
