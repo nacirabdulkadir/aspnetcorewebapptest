378919d750c32dac22cf1e8deeb7e9e0e4f5149b
nacirabdulkadir <121578916+nacirabdulkadir@users.noreply.github.com>
2023-11-16 22:47:53 +0300

    test

--- AspNetCoreWebAppTest/Controller/HomeController.cs
+++ AspNetCoreWebAppTest/Controller/HomeController.cs
@@ -17,8 +17,8 @@ namespace AspNetCoreWebAppTest
         public IActionResult Index()
         {
             return View();
-        }   
-         
+        }
+
         public string ConsumeKafkaMessage()
         {
             StringBuilder sb = new StringBuilder();
@@ -28,7 +28,7 @@ namespace AspNetCoreWebAppTest
                 BootstrapServers = _kafkaSettings.BootstrapServers,
                 GroupId = "test-consumer-group",
                 AutoOffsetReset = AutoOffsetReset.Earliest,
-                EnableAutoCommit=false
+                EnableAutoCommit = false
             };
 
             using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
@@ -47,6 +47,34 @@ namespace AspNetCoreWebAppTest
 
             return sb.ToString();
         }
+
+
+        [HttpPost]
+  
+        public async Task<IActionResult> ProducerKafkaMessage(string message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = _kafkaSettings.BootstrapServers
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var result = await producer.ProduceAsync(_kafkaSettings.TopicName, new Message<Null, string> { Value = message });
+                    return Ok($"Mesaj gönderildi: '{result.Value}' Topic: '{result.Topic}', Partition: '{result.Partition}', Offset: '{result.Offset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    return BadRequest($"Mesaj gönderilirken hata oluştu: {e.Error.Reason}");
+                }
+            }
+
+
+        }
+
+
     }
 
     public class KafkaSettings
@@ -54,4 +82,5 @@ namespace AspNetCoreWebAppTest
         public string BootstrapServers { get; set; }
         public string TopicName { get; set; }
     }
+
 }
