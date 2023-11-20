2962cc98c8aaedf23c6ee53d21d7174ff04ef423
nacirabdulkadir <121578916+nacirabdulkadir@users.noreply.github.com>
2023-11-21 00:55:49 +0300

    Cache temizlendi

--- AspNetCoreWebAppTest/Controller/HomeController.cs
+++ AspNetCoreWebAppTest/Controller/HomeController.cs
@@ -28,10 +28,11 @@ namespace AspNetCoreWebAppTest
             {
                 BootstrapServers = _kafkaSettings.BootstrapServers,
                 GroupId = "test-consumer-group",
-                AutoOffsetReset = AutoOffsetReset.Latest,
-                EnableAutoCommit = false
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = false,
+                
 
-            };
+            }; 
 
             using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
             {
