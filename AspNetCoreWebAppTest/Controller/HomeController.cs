7e66db61ad691e7fc5928f95ece680f7db537f08
nacirabdulkadir <121578916+nacirabdulkadir@users.noreply.github.com>
2023-11-27 01:10:22 +0300

    test

--- AspNetCoreWebAppTest/Controller/HomeController.cs
+++ AspNetCoreWebAppTest/Controller/HomeController.cs
@@ -40,19 +40,21 @@ namespace AspNetCoreWebAppTest
                 try
                 {
                     // Belirli bir sayıda mesajı oku veya belirli bir süre boyunca mesajları oku
-                    for (int i = 0; i < 10; i++) // Örneğin, en fazla 100 mesaj oku
+                    for (int i = 0; i < 3; i++) // Örneğin, en fazla 100 mesaj oku
                     {
                         var result = consumer.Consume();
+                        
                         if (result == null)
                             return sb.ToString();
 
-                        sb.AppendLine(result.Message.Value);
+                        sb.AppendLine($"Partition: {result.Partition} Mesaj: { result.Message.Value}");
                         consumer.Commit();
                     }
                 }
                 catch (ConsumeException e)
                 {
                     sb.AppendLine("Hata: " + e.Error.Reason);
+                    return sb.ToString();
                 }
             }
 
